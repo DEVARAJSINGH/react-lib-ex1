2294bde8594476d321b05ef2872b2505f2ce1375
devarajsingh <devarajsingh5@gmail.com>
2021-04-13 12:44:49 +0530

    +

---
+++ lib-test/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { TextLib } from 'dev-library';
+
+function App() {
+  return (
+    <div>
+      Hello <TextLib/>
+    </div>
+    );
+}
+
+export default App;
---
+++ src/component/text-lib.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function HelloWorld() {
+    return <h1>my first text lib</h1>
+}
+
+export default HelloWorld;
